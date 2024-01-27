295b1163f5d2819a5b176e90866a26767486d845
Krish Choudhary <kdhakla333@gmail.com>
2024-01-27 17:57:38 +0530

    Loading state

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
   @override
   void initState() {
@@ -46,6 +47,7 @@ class _GroceryListState extends State<GroceryList> {
     }
     setState(() {
       _groceryItems = loadedItems;
+      _isLoading = false;
     });
   }
 
@@ -79,6 +81,10 @@ class _GroceryListState extends State<GroceryList> {
     Widget content = const Center(
       child: Text("Empty list."),
     );
+
+    if (_isLoading) {
+      content = const Center(child: CircularProgressIndicator());
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
